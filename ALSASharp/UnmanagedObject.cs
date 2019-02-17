32b967a61024d90a4523c4a0ff3e462acb431115
Cezary Rojewski <cezary.j.rojewski@gmail.com>
2019-02-17 16:32:52 +0100

    Implement UnmanagedObject abstract class.
    UnmanagedObject class enforces declaration of Dispose method and
    declares internal IntPtr handle field, common to all types dealing with
    unmanaged code.

---
+++ ALSASharp/UnmanagedObject.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ALSASharp
+{
+    public abstract class UnmanagedObject : IDisposable
+    {
+        internal IntPtr handle;
+
+        public abstract void Dispose();
+    }
+}
