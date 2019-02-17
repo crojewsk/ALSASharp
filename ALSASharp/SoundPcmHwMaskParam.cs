e8efd4e619809f8b46435851303bf0334f0e153a
Cezary Rojewski <cezary.j.rojewski@gmail.com>
2019-02-17 16:40:00 +0100

    Implement SoundPcmHwMaskParam abstract class.
    SoundPcmHwMaskParam stands as a base class for all upcoming hw mask
    param related types. When inherited, enforces implementation of SetMask
    and GetMask methods, common to every hw mask param type.

---
+++ ALSASharp/SoundPcmHwMaskParam.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ALSASharp
+{
+    public abstract class SoundPcmHwMaskParam : SoundPcmHwBaseParam
+    {
+        protected SoundPcmHwMaskParam(IntPtr handle)
+            : base(handle)
+        {
+        }
+
+        public abstract int SetMask(SoundPcm pcm, UnmanagedObject mask);
+
+        public abstract UnmanagedObject GetMask();
+    }
+}
