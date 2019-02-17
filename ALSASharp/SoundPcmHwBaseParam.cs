b72defd8cb5a640be6855e4ae39d2e455a3da78c
Cezary Rojewski <cezary.j.rojewski@gmail.com>
2019-02-17 16:22:26 +0100

    Implement SoundPcmHwBaseParam abstract class.
    SoundPcmHwBaseParam stands as a base class for all upcoming hw param
    related types. When inherited, enforces implementation of methods common
    to every hw param type.

---
+++ ALSASharp/SoundPcmHwBaseParam.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ALSASharp
+{
+    public abstract class SoundPcmHwBaseParam
+    {
+        protected IntPtr owner;
+
+        protected SoundPcmHwBaseParam(IntPtr handle)
+        {
+            owner = handle;
+        }
+
+        public abstract int Get(out uint value, int dir = 0);
+
+        public abstract int Test(SoundPcm pcm, uint value, int dir = 0);
+
+        public abstract int Set(SoundPcm pcm, uint value, int dir = 0);
+
+        public abstract int SetFirst(SoundPcm pcm, out uint value, int dir = 0);
+
+        public abstract int SetLast(SoundPcm pcm, out uint value, int dir = 0);
+    }
+}
