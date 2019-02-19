e9559f6ce436f0f8366dff180adf6881b73e42d5
Cezary Rojewski <cezary.j.rojewski@gmail.com>
2019-02-19 21:32:58 +0100

    Implement SoundPcmHwIntervalParam class.
    SoundPcmHwIntervalParam stands as a base class for all upcoming hw
    inteval param related types. When inherited, enforces implementation
    of several methods, common to every hw interval param type.

---
+++ ALSASharp/SoundPcmHwIntervalParam.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ALSASharp
+{
+    public abstract class SoundPcmHwIntervalParam : SoundPcmHwBaseParam
+    {
+        protected SoundPcmHwIntervalParam(IntPtr handle)
+            : base(handle)
+        {
+        }
+
+        public abstract int GetMin(out uint value, int dir = 0);
+
+        public abstract int GetMax(out uint value, int dir = 0);
+
+        public abstract int SetMin(SoundPcm pcm, ref uint value, int dir = 0);
+
+        public abstract int SetMax(SoundPcm pcm, ref uint value, int dir = 0);
+
+        public abstract int SetMinMax(SoundPcm pcm, ref uint min, ref uint max, int mindir = 0, int maxdir = 0);
+
+        public abstract int SetNear(SoundPcm pcm, ref uint value, int dir = 0);
+
+        public abstract int SetInteger(SoundPcm pcm);
+    }
+}
